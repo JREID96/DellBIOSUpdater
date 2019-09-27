06764e9c6a5b71e47c675329d7d3d1ecf3b13612
Jarred Reid <51389325+JREID96@users.noreply.github.com>
2019-09-27 16:46:29 -0400

    Update DellBIOSUpdater.ps1
    Added support for Dell 3070
    Removed multiple switch statements in lieu of a single switch function

--- DellBIOSUpdater.ps1
+++ DellBIOSUpdater.ps1
@@ -1,14 +1,17 @@
-### Dell BIOS Updater v2.0
-### Automatically updates the BIOS of Dell machines. Files are pulled from \\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\
-### You can use the DellBIOSUpdater.exe utility to upload updated Dell BIOS executables (Found inside the same directory)
-### Jarred Reid 2019
+<# 
+Dell BIOS Updater v2.1.1
+Automatically updates the BIOS of Dell machines. Files are pulled from \\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\
+You can use the DellBIOSUpdater.exe utility to upload updated Dell BIOS executables (Found inside the same directory)
+Jarred Reid 2019
 
-### Changelog: 
+Changelog: 
 
-### 2.0 - Added support for Optiplex 7060s
-### 1.9 - Fixed a bug where the script would not run if "newbios.exe" was present in C:\Temp\
-### 1.8 - Added support for Precision 5820 Tower
-### 1.7 - Added support for Optiplex 3060s
+2.1.1 - Moved logic from multiple "if" statements over to a single switch parameter and added support for Dell Optiplex 3070
+2.0 - Added support for Optiplex 7060s
+1.9 - Fixed a bug where the script would not run if "newbios.exe" was present in C:\Temp\
+1.8 - Added support for Precision 5820 Tower
+1.7 - Added support for Optiplex 3060s 
+#>
 
 Clear-Host
 
@@ -28,44 +31,50 @@ if(Test-Path $bios)
     Remove-Item $bios
 }
 
-### Downloads the correct firmware based on the $Model variable, then renames it to "newbios.exe"
-if ($Model -eq "Optiplex 3060"){
-    Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3060.exe" -Destination "C:\Temp\"
-    Rename-Item -Path "C:\Temp\newbios3060.exe" -NewName "newbios.exe"
-}
+switch ($model) {
+    "Optiplex 3020" {
+        Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3020.exe" -Destination "C:\Temp\" 
+        Rename-Item -Path "C:\Temp\newbios3020.exe" -NewName "newbios.exe"
+    }
 
-if ($Model -eq "OptiPlex 3050"){
-    Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3050.exe" -Destination "C:\Temp\"
-    Rename-Item -Path "C:\Temp\newbios3050.exe" -NewName "newbios.exe"
-}
-
-if ($Model -eq "OptiPlex 3040"){
-    Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3040.exe" -Destination "C:\Temp\" 
+    "Optiplex 3040" {
+        Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3040.exe" -Destination "C:\Temp\" 
     Rename-Item -Path "C:\Temp\newbios3040.exe" -NewName "newbios.exe"
-}
+    }
 
-if ($Model -eq "OptiPlex 3020"){
-    Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3020.exe" -Destination "C:\Temp\" 
-    Rename-Item -Path "C:\Temp\newbios3020.exe" -NewName "newbios.exe"
-}
+    "Optiplex 3050" {
+        Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3050.exe" -Destination "C:\Temp\"
+        Rename-Item -Path "C:\Temp\newbios3050.exe" -NewName "newbios.exe"
+    }
 
-if ($Model -eq "OptiPlex 7050"){
-    Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios7050.exe" -Destination "C:\Temp\" 
-    Rename-Item -Path "C:\Temp\newbios7050.exe" -NewName "newbios.exe"
-}
+    "Optiplex 3060" {
+        Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3060.exe" -Destination "C:\Temp\"
+        Rename-Item -Path "C:\Temp\newbios3060.exe" -NewName "newbios.exe"
+    }
 
-if ($Model -eq "OptiPlex 7060"){
-    Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios7060.exe" -Destination "C:\Temp\" 
-    Rename-Item -Path "C:\Temp\newbios7060.exe" -NewName "newbios.exe"
-}
+    "Optiplex 3070" {
+        Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios3060.exe" -Destination "C:\Temp\"
+        Rename-Item -Path "C:\Temp\newbios3060.exe" -NewName "newbios.exe"
+    }
 
-if ($Model -eq "Precision 5820 Tower"){
-    Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios5820.exe" -Destination "C:\Temp\" 
-    Rename-Item -Path "C:\Temp\newbios5820.exe" -NewName "newbios.exe"
+    "Optiplex 7050" {
+        Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios7050.exe" -Destination "C:\Temp\" 
+        Rename-Item -Path "C:\Temp\newbios7050.exe" -NewName "newbios.exe"
+    }
+
+    "Optiplex 7060" {
+        Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios7060.exe" -Destination "C:\Temp\" 
+        Rename-Item -Path "C:\Temp\newbios7060.exe" -NewName "newbios.exe"
+    }
+
+    "Precision 5820 Tower" {
+        Copy "\\fileserver\UserShares\Dept-IT\ServiceCatalog-Technical\_Drivers-Firmware\Dell\DellBIOSUpdater\newbios5820.exe" -Destination "C:\Temp\" 
+        Rename-Item -Path "C:\Temp\newbios5820.exe" -NewName "newbios.exe"
+    }
 }
 
 ### Suspends Bitlocker on the drive, and will re-encrypt after 1 reboot.
 Suspend-BitLocker -MountPoint C: -RebootCount 1 
 
 ### Silently starts the BIOS update, and gives the machine the 1 reboot it needs to re-encrypt the drive.
-C:\Temp\newbios.exe /s /r /p=
+C:\Temp\newbios.exe /s /r /p=BIOSdome480
